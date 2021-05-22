eb52243d77fea9051c95a5d28ae7f581a1a3ce1c
Igbal032 <iqbal.hoff@list.ru>
2021-05-22 19:37:20 +0400

    Phone Regax elave etdim

--- SearchingSystem/src/main/java/student/inputs/StringInput.java
+++ SearchingSystem/src/main/java/student/inputs/StringInput.java
@@ -12,15 +12,21 @@ import java.util.regex.Pattern;
 public class StringInput {
 
     private static final String EMAIL_REGEX = "([a-zA-Z0-9_.+-])+\\@(([a-zA-Z0-9-])+\\.)+([a-zA-Z0-9]{2,4})";
+    private static final String PHONE_REGEX = "[+]{1}[9]{2}[4]{1}(([5]([0]|[1]|[5]))|([7]([0]|[7]))|([9]([9])))[1-9][0-9]{6}";
 
-    public static String addString(String text) {
-        String myString;
+    public static String addString(String text,boolean isPhone) {
+        String myString="";
         do {
             System.out.print(ConsoleColors.BLUE+text+ConsoleColors.RESET);
             Scanner scanner = new Scanner(System.in);
             myString = scanner.nextLine();
+            if (isPhone==true){
+                if (!isValidEmailOrPhone(myString, PHONE_REGEX)) {
+                    System.out.println(ConsoleColors.RED+"Nomrenizi dogru sekilde daxil edin.(+994551234567)"+ConsoleColors.RESET);
+                }
+            }
 
-        } while (myString.length() == 0);
+        } while ((!isValidEmailOrPhone(myString, PHONE_REGEX)&&isPhone==true)||myString.length() == 0);
         return myString;
     }
 
@@ -32,20 +38,22 @@ public class StringInput {
             Scanner scanner = new Scanner(System.in);
             myString = scanner.nextLine();
 
-            if (!isValid(myString, EMAIL_REGEX)) {
+            if (!isValidEmailOrPhone(myString, EMAIL_REGEX)) {
                 System.out.println(ConsoleColors.RED+"Emailinizi dogru sekilde daxil edin."+ConsoleColors.RESET);
             }
             if (implemantations.getStudentListByEmail().contains(myString)){
                 System.out.println(ConsoleColors.RED+"Bu adda email artiq var"+ConsoleColors.RESET);
             }
 
-        } while (!isValid(myString, EMAIL_REGEX) || implemantations.getStudentListByEmail().contains(myString));
+        }
+        while (!isValidEmailOrPhone(myString, EMAIL_REGEX) || implemantations.getStudentListByEmail().contains(myString));
         return myString;
+
     }
 
-    public static boolean isValid(String email, String regex) {
+    public static boolean isValidEmailOrPhone(String emailOrNumber, String regex) {
         Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(email);
+        Matcher matcher = pattern.matcher(emailOrNumber);
         return matcher.matches();
     }
 
